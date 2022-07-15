f44f07af2906d160746a30a689f1a88da1c9abb5
Roger Yuri <rogerllima0@gmail.com>
2022-07-14 23:21:45 -0300

    Adição da função emprestimo e extrato bancário

--- Tela Login/index.js
+++ Tela Login/index.js
@@ -19,18 +19,56 @@ function efetuarLogin() {
     location.href = '../Tela Menu/tela-menu.html';
 
 }
+
+function efetuarLogout(){
+    localStorage.clear();
+    location.href = '../Tela Login/tela-login.html';
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
     let user = JSON.parse(localStorage.getItem("user"));
-    document.getElementById("nome").innerHTML = user.nomeUsuario;
-    document.getElementById("conta").innerHTML = user.numeroConta;
-    document.getElementById("saldo").innerHTML = user.saldo;
+    document.getElementById("nome").innerHTML = ('Nome: '+user.nomeUsuario);
+    document.getElementById("conta").innerHTML = ('Número da conta: '+user.numeroConta);
+    document.getElementById("saldo").innerHTML = ('Saldo: R$'+user.saldo);
 
 });
+
 function mostrarConteudo(id) {
+    //  Capturar status do painel a ser modificado
+    let primeiroStyle = document.getElementById(id).style.display;
+    //  Desativar todos os paineis
+    let paineis = document.getElementsByClassName('painel');
+    for (var i = 0; i < paineis.length; i++) {
+        paineis[i].style.display = 'none';
+    };
+    //  Alternar status original do painel a ser modificado
     const targetDiv = document.getElementById(id);
-    if (targetDiv.style.display != "none") {
+    if (primeiroStyle != "none") {
         targetDiv.style.display = "none";
     } else {
         targetDiv.style.display = "block";
     }
+};
+
+function simularEmprestimo() {
+    let valorEmprestimo = parseInt(document.getElementById('valorEmprestimo').value);
+    let numeroParcelas = parseInt(document.getElementById('parcelasEmprestimo').value);
+    let juros = 0.0314;
+    let valorJurosInicial = valorEmprestimo * juros;
+    let novoValorJuros = 0
+    let valorJurosFinal = 0;
+    let valorFinalParcelado = 0;
+    for (let i = 0; i < numeroParcelas; i++) {
+        if (i == 0) {
+            novoValorJuros = valorEmprestimo + valorJurosInicial;;
+            valorJurosFinal = novoValorJuros;
+        } else {
+            novoValorJuros = valorJurosFinal*juros;
+            valorJurosFinal = valorJurosFinal+novoValorJuros;
+        }
+        valorFinalParcelado = valorJurosFinal/numeroParcelas;
+    }
+    console.log(novoValorJuros);
+    document.getElementById('valorSimulado').innerHTML = ('R$'+valorJurosFinal.toFixed(2).toString());
+    document.getElementById('valorParcela').innerHTML = ('R$'+valorFinalParcelado.toFixed(2).toString());
 };
